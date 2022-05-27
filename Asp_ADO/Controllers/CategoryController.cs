bdbe60c29d2561f1b8c2d7ca94100baab1d43211
Khanhld <khanhld0211@gmail.com>
2022-05-27 14:01:40 +0700

    first

---
+++ Asp_ADO/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using Asp_ADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp_ADO.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private Asp_ADOEntities db;
+        public CategoryController()
+        {
+            db = new Asp_ADOEntities();
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            var cat = db.Category.ToList();
+            return View(cat);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category c)
+        {
+            db.Category.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            return View(db.Category.Find(id));
+        }
+        public ActionResult Edit(int id)
+        {
+            var cat = db.Category.Find(id);
+            return View(cat);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category c)
+        {
+            db.Entry(c).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var cat = db.Category.Find(id);
+            db.Category.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Asp_ADO/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Asp_ADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp_ADO.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Asp_ADOEntities db;
+        public ProductController()
+        {
+            db = new Asp_ADOEntities();
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            var pro = db.Product.AsEnumerable();
+            return View(pro);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Product p)
+        {
+
+            db.Product.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string id)
+        {
+            var pro = db.Product.SingleOrDefault(x => x.ProId == id);
+            return View(pro);
+        }
+        public ActionResult Edit(string id)
+        {
+            var pro = db.Product.SingleOrDefault(x => x.ProId == id);
+            return View(pro);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            var pro = db.Product.Find(id);
+            db.Product.Remove(pro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
